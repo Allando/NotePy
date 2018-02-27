b153e85d1cb0b42c7568c1cbcabd5473c46b048b
Allando <aln-nielsen@protonmail.com>
2018-02-27 12:01:19 +0100

    ayy

--- notes.py
+++ notes.py
@@ -8,20 +8,34 @@ class Notes:
 
     @staticmethod
     def add_note(title, note, time=None):
-        with open('notes.json', 'a') as fw:
-            if time == None:
-                time = "na"
-            
-            note = {"Title": title,
-                    "Note": note,
-                    "Notification time": time}
-   
-            fw.write(json.dump(note, fw))
-        fw.close()
+        _id = 0
+        idList = []
 
-    def remove_note(self):
-        pass
+        try:
+            with open('notes.json', 'r') as rw:
+                for i in rw:
+                    for "Id" in i:
+                        idList.append("Id")
+                        r = idList[ len(idList) -1 ]
+                        _id = r + 1
+        except:
+            _id = 0
 
-    def edit_note(self):
-        pass
+with open('notes.json', 'a') as fw:
+    if time == None:
+        time = "na"
+
+    note = {"Id": _id,
+            "Title": title,
+            "Note": note,
+            "Notification time": time}
+
+    fw.write(json.dump(note, fw))
+fw.close()
+
+def remove_note(self):
+    pass
+
+def edit_note(self):
+    pass
 
